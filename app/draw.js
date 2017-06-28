5daddea154bab8170e5fe5ac1d8b682c96163803
kwen <stralipch@gmail.com>
2017-06-28 13:22:46 +0900

    https

--- app/draw.js
+++ app/draw.js
@@ -75,7 +75,7 @@ function hideDetails() {
 function colorMap(axis) {
     var date = Object.values(axis.calendar).join('');
     var hour = axis.hour;
-    var filepath = "http://os8iv8668.bkt.clouddn.com/" + date + hour + ".json";
+    var filepath = "https://o7d1qohdm.qnssl.com/" + date + hour + ".json";
     d3.json(filepath, function(err, root) {
         if(root) { // 有数据
             G.aqd = root;
